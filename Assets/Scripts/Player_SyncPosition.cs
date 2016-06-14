a85115c0e1e55e526a4a2e8447bd267df4af2d80
DeadlinezZ <fabrice-95@hotmail.com>
2016-06-14 16:01:58 +0200

    [Added] Position & Rotation Syncing Across the Network
    + Position & Rotation Syncing
    Using Lerp functions and UNET Commands + SyncVar to keep the positions
    and rotations up to date and smooth, everything adjustable in the editor

---
+++ Assets/Scripts/Player_SyncPosition.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+
+public class Player_SyncPosition : NetworkBehaviour
+{
+    
+    [SyncVar]
+    private Vector3 syncPos;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float lerpRate = 15;
+    private Vector3 lastPosition;
+
+    public float updateDistanceThreshold = 0.1f;
+
+    void Start()
+    {
+        lastPosition = playerTransform.position;
+    }
+
+
+    void Update()
+    {
+        LerpPosition();
+    }
+
+    void FixedUpdate()
+    {
+        SendPosition();
+    }
+
+    void LerpPosition()
+    {
+        //Only happens on other players objects since we ourself are moving smooth already
+        if (!isLocalPlayer)
+        {
+            playerTransform.position = Vector3.Lerp(playerTransform.position, syncPos, Time.deltaTime*lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdSendPositionToserver(Vector3 pos)
+    {
+        //Client sends its position to server using Command on every movement
+        syncPos = pos;
+    }
+
+    [ClientCallback]
+    void SendPosition()
+    {
+        if (isLocalPlayer && Vector3.Distance(playerTransform.position, lastPosition) >= updateDistanceThreshold)
+        {
+            lastPosition = playerTransform.position;
+            CmdSendPositionToserver(playerTransform.position);
+        }
+    }
+}
+    
---
+++ Assets/Scripts/Player_SyncRotation.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_SyncRotation : NetworkBehaviour
+{
+
+    [SyncVar] private Quaternion syncPlayerRotation;
+    [SyncVar] private Quaternion syncCamRotation;
+
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform camTransform;
+    [SerializeField] private float lerpRate = 15;
+
+    private Quaternion lastPlayerRot;
+    private Quaternion lastCamRot;
+    public float rotationUpdateThreshhold = 0.1f;
+
+    void Update()
+    {
+        LerpRotations();
+    }
+
+    void FixedUpdate()
+    {
+        SendRotations();
+    }
+
+    void LerpRotations()
+    {
+        if (!isLocalPlayer)
+        {
+            playerTransform.rotation = Quaternion.Lerp(playerTransform.rotation, syncPlayerRotation, Time.deltaTime * lerpRate);
+            camTransform.rotation = Quaternion.Lerp(camTransform.rotation, syncCamRotation, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdSendRotationsToServer(Quaternion playerRot, Quaternion camRot)
+    {
+        syncPlayerRotation = playerRot;
+        syncCamRotation = camRot;
+    }
+
+    [ClientCallback]
+    void SendRotations()
+    {
+        if (isLocalPlayer && (Quaternion.Angle(playerTransform.rotation, lastPlayerRot) > rotationUpdateThreshhold || Quaternion.Angle(camTransform.rotation, lastCamRot) > rotationUpdateThreshhold))
+        {
+            CmdSendRotationsToServer(playerTransform.rotation, camTransform.rotation);
+            lastPlayerRot = playerTransform.rotation;
+            lastCamRot = camTransform.rotation;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
